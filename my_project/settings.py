4a65cdd8448920f7b402b8d66770349fd63e1fa7
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-07-09 14:13:17 +0100

    Revert debug in settins.py to true

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n%&zw51*s8puk-&t7b*ushzkvx35rtyz*x&0ci&v5s9zqbr54$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
